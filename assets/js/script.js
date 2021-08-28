bd4a84e30b37b40fb5e7d75ff9f85d8f22e5087c
Salim66 <salimhasanriad8@gmail.com>
2021-08-28 09:39:54 +0600

    All design and javascript functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,33 @@
 // grab everything we need
-
+const scrollLine = document.querySelector('.scroll-line');
 
 // define all function w'll need
+function fillScrollLine(){
+    const window_height = window.innerHeight;
+    const full_height   = document.body.clientHeight;
+    const scrolled = window.scrollY;
+    let percent_scrolled = (scrolled / (full_height - window_height)) * 100;
 
+    scrollLine.style.width = `${percent_scrolled}%`;
+}
 
 // add event listeners 
+window.addEventListener('scroll', fillScrollLine);
+window.addEventListener('scroll', debounce(function(){
+
+}, 20));
+
+function debounce(func, wait=15, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
